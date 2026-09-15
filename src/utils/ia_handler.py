684e1815fd3bc78e3c05e82abf4f42ab246e1052
Gustavo <gustavoadutra.contato@gmail.com>
2026-09-14 22:13:39 -0300

    feat: enhance IA response prompt and update RAG to use new index and metadata structure

--- src/utils/ia_handler.py
+++ src/utils/ia_handler.py
@@ -9,12 +9,18 @@ class IA:
         API = os.getenv("GEMINI_API_KEY")
 
         self.client = genai.Client(api_key=API)
+        self.prompt = f""" 
+        Você é um fisioterapeuta e deve ajudar outro fisioterapeuta utilizando as fontes,
+        responda a pergunta em português brasil, não invente informações, 
+        se achar que algum texto da fonte não ajuda na resposta apenas ignore ele, 
+        não use markdown na resposta. Cite as fontes como se fosse um artigo científico. 
+        No final das fontes coloque quais as páginas usadas.
+        """
 
     def response(self, question, chunks):
         interaction = self.client.interactions.create(
             model="gemini-3.8-flash",
-            input="Você é um fisioterapeuta e deve ajudar outro fisioterapeuta utilizando as fontes, responda a pergunta em português brasil, não invente informações,"
-            + f"se achar que algum texto da fonte não ajuda na resposta apenas ignore ele, não use markdown na resposta. Pergunta {question} Fontes: {chunks}"
+            input= f"{self.prompt} Pergunta {question} Fontes: {chunks}"
         )
         print(interaction.output_text)
         print("=="*20)
